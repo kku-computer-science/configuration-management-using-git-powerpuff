da8be640a663c5fa60d36fd7931600b83686cabd
Kamonwan04 <kamonwan.d@kkumail.com>
2025-12-07 18:04:19 +0700

    Created BubbleSort.java

---
+++ Project/BubbleSort.java
@@ -0,0 +1,35 @@
+public class BubbleSort {
+
+    public static void bubbleSort(int[] arr) {
+        int n = arr.length;
+        int steps = 0;
+
+        for (int i = 0; i < n - 1; i++) {
+            boolean swapped = false;
+
+            for (int j = 0; j < n - i - 1; j++) {
+                steps++;
+                if (arr[j] > arr[j + 1]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                    swapped = true;
+                }
+            }
+
+            if (!swapped) {
+                break;
+            }
+        }
+
+        System.out.print("Bubble sort: ");
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i]);
+            if (i < arr.length - 1) {
+                System.out.print(", ");
+            }
+        }
+        System.out.println(";");
+        System.out.println("Steps: " + steps);
+    }
+}
